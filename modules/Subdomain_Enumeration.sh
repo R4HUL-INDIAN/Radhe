a98240c6997a4f5b2498470801286ad319458ca3
R4HUL-INDIAN <106301889+R4HUL-INDIAN@users.noreply.github.com>
2023-07-10 17:48:52 +0530

    Update Subdomain_Enumeration.sh

--- modules/Subdomain_Enumeration.sh
+++ modules/Subdomain_Enumeration.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+clear 
+
+start_time=$(date +%s)  # Record the start time
+
+# M𝙖𝙙𝙚 𝙬𝙞𝙩𝙝 🖤 𝙗𝙮 𝙍4𝙃𝙐𝙇-𝙄𝙉𝘿𝙄𝘼𝙉
+
 echo  # For one-line gap
 
-echo -e "\e[0;36mRADHE RADHE ❤ \e[0m" 
+echo -e "\e[1;33mRADHE RADHE ❤ \e[0m" 
 
 echo  # For one-line gap
 
@@ -10,16 +16,46 @@ read -p "Enter the Target Name: " domain
 
 echo  # For one-line gap
 
-amass enum -passive -d $domain -o amass.txt 
+echo -e "\e[1;33m[+] Collecting Subdomains From Amass \e[0m"
+
+amass enum -passive -d $domain -o amass.txt >/dev/null 2>&1
+
+
+echo  # For one-line gap
+
+echo -e "\e[1;33m[+] Collecting Subdomains From Assetfinder \e[0m"
 
 assetfinder --subs-only $domain > assetfinder.txt
 
-subfinder -d $domain > subfinder.txt
+echo  # For one-line gap
 
-sublist3r -d $domain  -o sublist3r.txt
+echo -e "\e[1;33m[+] Collecting Subdomains From Subfinder \e[0m"
 
-cat amass.txt assetfinder.txt subfinder.txt sublist3r.txt >> subdomains.txt 
+subfinder -d $domain 2>/dev/null > subfinder.txt
 
-rm amass.txt assetfinder.txt subfinder.txt sublist3r.txt
+echo  # For one-line gap
 
-echo -e "\e[0;36mSUBDOMAINS ENUMERATION FINISHED :) \e[0m"
+echo -e "\e[1;33m[+] Collecting Subdomains From Sublist3r \e[0m"
+
+# sublist3r -d $domain  -o sublist3r.txt >/dev/null 2>&1
+
+cat amass.txt assetfinder.txt subfinder.txt >> subdomains.txt 
+
+rm amass.txt assetfinder.txt subfinder.txt 
+
+echo  # For one-line gap
+
+# For Script Execution Timing
+
+end_time=$(date +%s)  # Record the end time
+execution_time=$((end_time - start_time))  
+
+if ((execution_time < 60)); then
+  echo "The Subdomain Enumeration Module finished in $execution_time seconds"
+else
+  minutes=$((execution_time / 60))  
+  seconds=$((execution_time % 60))  
+  echo "The Subdomain Enumeration Module finished in $seconds seconds"
+fi
+
+# M𝙖𝙙𝙚 𝙬𝙞𝙩𝙝 🖤 𝙗𝙮 𝙍4𝙃𝙐𝙇-𝙄𝙉𝘿𝙄𝘼𝙉
